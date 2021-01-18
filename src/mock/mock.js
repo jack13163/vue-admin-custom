0a945ea71f676cf3fc757406f89884f140b1f562
jack13163 <18163132129@163.com>
2021-01-18 18:27:32 +0800

    update

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -21,7 +21,6 @@ Mock.mock('http://127.0.0.1:9960/test/user', 'get', () => {
 // post请求,带参数,参数会在data中返回,会返回url,type,body三个参数,可以把data打印出来看看
 Mock.mock('http://127.0.0.1:9960/test/user', 'post', (data) => {
     var id = JSON.parse(data.body)['id'];
-    console.log(id)
     // 利用Mockjs自定义返回的内容
     return [{
         id: id,
